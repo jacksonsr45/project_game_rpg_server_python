49c4c74a64f04f922232c40b8193ad2db581e3a8
Jackson <jacksonsr45@gmail.com>
2020-03-01 19:55:45 -0300

    add file new_enemy.py in define_enemies

---
+++ server/src/define_enemies/new_enemy.py
@@ -0,0 +1,9 @@
+
+class New_Enemy:
+    def __init__(self, new_enemy):
+        enemy_name = new_enemy['class_name']
+        enemy_level = new_enemy['pos_map']
+        enemy_amount = new_enemy['amount']
+
+    def create_new_enemy(self):
+        pass
